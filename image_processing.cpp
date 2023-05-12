f6e2eb5d319a5f4cb3bd61b7480578473c2d7a54
raspi-sdk <marco.odermatt.01@stud.hslu.ch>
2023-05-12 20:21:24 +0200

    version 1.0 change detection

--- image_processing.cpp
+++ image_processing.cpp
@@ -24,14 +24,60 @@ cv::Mat* CImageProcessor::GetProcImage(uint32 i) {
 }
 
 int CImageProcessor::DoProcess(cv::Mat* image) {
+
+	cv::Mat grayImage;
 	
 	if(!image) return(EINVALID_PARAMETER);	
         
-        
+		//	make gray	
+
+        if(image->channels()>1) {
+			cv::cvtColor(*image, grayImage, cv::COLOR_RGB2GRAY);	//if color image, convert to grayscale img
+		}
+		else {
+			grayImage = *image;
+		}
+
+
+
+		cv::Mat diffImage;
+		cv::Mat mask;
+
+		if (mPrevImage.size() != cv::Size())	//is data fine? has it allready passed once
+		{
+			//mPrevImage = grayImage;
+			//mPrevImage *= 0;
+			//mPrevImage += (0.5 * 128);
+
+			cv::absdiff(mPrevImage, *image, diffImage);	//create difference between this and last image
+			//diffImage = cv::abs(mPrevImage - *image);	//same procut, just slightly different code
+
+			cv::abs(diffImage);
+
+
+			//modify diff image so noise is not present
+			diffImage.at<uint8>(0,0)=255;		//set one pixel to max value
+			diffImage.at<uint8>(1,0)=0;			//set one pixel to min value
+		}
+		else
+		{
+		}
+
+		mPrevImage = grayImage.clone();		//generate image for next round
+
+
+
+
 
 
         cv::subtract(cv::Scalar::all(255), *image,*m_proc_image[0]);
+		*m_proc_image[1] = grayImage;
+		*m_proc_image[2] = diffImage;
+		
+
+
         
+		// use *m_proc_image[ ... ] 0 up to 2 for output image
       //  cv::imwrite("dx.png", *m_proc_image[0]);
       //  cv::imwrite("dy.png", *m_proc_image[1]);
 
--- image_processing.h
+++ image_processing.h
@@ -23,6 +23,9 @@ public:
 
 private:
 	cv::Mat* m_proc_image[3];/* we have three processing images for visualization available */
+	
+	cv::Mat mPrevImage;
+
 };
 
 
