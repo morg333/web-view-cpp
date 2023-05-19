70f48f53adf13ebe7173959d64bb280a4fe68a04
raspi-sdk <marco.odermatt.01@stud.hslu.ch>
2023-05-19 15:46:57 +0200

    added change detection

--- image_processing.cpp
+++ image_processing.cpp
@@ -65,19 +65,76 @@ int CImageProcessor::DoProcess(cv::Mat* image) {
 			diffImage = *image * 0;		//output at least a black image with correct pixel dimensions
 		}
 
+
+
+
+
+	// -- init -- because if defined in IF statement, it might not have been called before...
+		//init for binary Image
+		cv::Mat binaryImage;
+		//init for region Labeling
+		cv::Mat labelImage;
+		//init for bounding box image
+		cv::Mat resultImage = image->clone();
+
+		if(mPrevImage.size() != cv::Size()){
+			//image Processing
+
+		// ---- a) Difference Image ----
+			cv::Mat diffImage;
+			cv::absdiff(*image, mPrevImage, diffImage);
+
+		// ---- b) Binarisierung ----
+			double threshold = 50;
+			cv::threshold(diffImage, binaryImage, threshold, 255, cv::THRESH_BINARY);
+
+		// ---- c) Morphologie ----
+			cv::Mat kernel = cv::Mat::ones(5, 5, CV_8UC1);
+			cv::morphologyEx(binaryImage, binaryImage, cv::MORPH_CLOSE, kernel);
+
+		// ---- d) Region Labeling ----
+			cv::Mat stats, centroids;
+			connectedComponentsWithStats(binaryImage, labelImage, stats, centroids);
+
+			//access values from Labeled Images data
+			for (int i = 1; i < stats.rows; i++) {
+			int topLeftx = stats.at<int>(i, 0);
+			int topLefty = stats.at<int>(i, 1);
+			int width = stats.at<int>(i, 2);
+			int height = stats.at<int>(i, 3);
+			int area = stats.at<int>(i, 4);
+			double cx = centroids.at<double>(i, 0);
+			double cy = centroids.at<double>(i, 1);
+
+			//draw bounding box over image copy
+			cv::Rect rect(topLeftx, topLefty, width, height);
+			cv::rectangle(resultImage, rect, cv::Scalar(255, 0, 0));
+			//draw centerPoint onto image
+			cv::Point2d cent(cx, cy);
+			cv::circle(resultImage, cent, 5, cv::Scalar(128, 0, 0), -1);
+
+			//..do sth more with values..
+			}
+
+		// ---- Image ----
+
+		// ---- Image ----
+			
+
+		}
 		mPrevImage = grayImage.clone();		//generate image for next round
 
 
+	// ---- 3) Visualisierung ---
+		*m_proc_image[0] = labelImage;
+		*m_proc_image[1] = binaryImage;
+		*m_proc_image[2] = resultImage;
 
 
-
-
-        cv::subtract(cv::Scalar::all(255), *image,*m_proc_image[0]);
-		*m_proc_image[1] = grayImage;
-		*m_proc_image[2] = diffImage;
-		
-
-
+//testing
+        //cv::subtract(cv::Scalar::all(255), *image,*m_proc_image[0]);
+		//*m_proc_image[1] = grayImage;
+		//*m_proc_image[2] = diffImage;
         
 		// use *m_proc_image[ ... ] 0 up to 2 for output image
       //  cv::imwrite("dx.png", *m_proc_image[0]);
